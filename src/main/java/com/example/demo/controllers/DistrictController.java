3878a563032a6b486901cef82bc77e6df87594a7
Javier Pérez <javier.perez.ca@usach.cl>
2019-09-22 22:22:37 -0300

    Fix: Creación de Distrito

--- src/main/java/com/example/demo/controllers/DistrictController.java
+++ src/main/java/com/example/demo/controllers/DistrictController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.City;
 import com.example.demo.models.District;
+import com.example.demo.repositories.CityRepository;
 import com.example.demo.repositories.DistrictRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -13,6 +18,8 @@ public class DistrictController {
 
     @Autowired
     DistrictRepository districtRepository;
+    @Autowired
+    CityRepository cityRepository;
 
     @GetMapping("/districts")
     @ResponseBody
@@ -28,4 +35,25 @@ public class DistrictController {
     @ResponseBody
     public District getCityByName(@PathVariable String name) { return districtRepository.findDistrictByName(name); }
 
+    @PostMapping("/districts/create")
+    @ResponseBody
+    public List<HashMap<String, String>> insertDistrict(@RequestBody Map<String, Object> jsonData) {
+        List<HashMap<String, String>> result = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map = new HashMap<>();
+        Long idCity = Long.parseLong(jsonData.get("city").toString());
+        City city =  cityRepository.findCityById(idCity);
+        if(city != null){
+            districtRepository.save(new District(jsonData.get("name").toString(),city));
+            map.put("status", "201");
+            map.put("message", "District added");
+            result.add(map);
+            return result;
+        }
+        else {
+            map.put("status", "401");
+            map.put("message", "User does not exist!.");
+            result.add(map);
+            return result;
+        }
+    }
 }
